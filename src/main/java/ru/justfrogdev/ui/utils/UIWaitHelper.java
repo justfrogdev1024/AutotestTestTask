3e9ca7ff2b69e4d17a082ff4993a4f8c7ca82da1
Archer1024 <archer1024@github.com>
2026-03-03 13:59:55 +0000

    fix: добавлен импорт TimeoutException в UIWaitHelper
    Ошибка: Cannot resolve symbol 'TimeoutException'
    Решение: import org.openqa.selenium.TimeoutException;

--- src/main/java/ru/justfrogdev/ui/utils/UIWaitHelper.java
+++ src/main/java/ru/justfrogdev/ui/utils/UIWaitHelper.java
@@ -1,6 +1,7 @@
 package ru.justfrogdev.ui.utils;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
